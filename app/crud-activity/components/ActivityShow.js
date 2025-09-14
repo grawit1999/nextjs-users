2ee4abb919811aa5f79d7dc1d3ed0c7ac8a9eb32
Game Rawit <g.rawit1999@gmail.com>
2025-09-14 13:06:06 +0700

    add crud-activity

---
+++ app/crud-activity/components/ActivityShow.js
@@ -0,0 +1,221 @@
+import * as React from 'react';
+import Alert from '@mui/material/Alert';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import CircularProgress from '@mui/material/CircularProgress';
+import Divider from '@mui/material/Divider';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { useNavigate, useParams } from 'react-router';
+import dayjs from 'dayjs';
+import { useDialogs } from '../hooks/useDialogs/useDialogs';
+import useNotifications from '../hooks/useNotifications/useNotifications';
+import {
+  deleteOne as deleteActivity,
+  getOne as getActivity,
+} from '../data/activities';
+import PageContainer from './PageContainer';
+
+export default function ActivityShow() {
+  const { activityId } = useParams();
+  const navigate = useNavigate();
+
+  const dialogs = useDialogs();
+  const notifications = useNotifications();
+
+  const [activity, setActivity] = React.useState(null);
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [error, setError] = React.useState(null);
+
+  const loadData = React.useCallback(async () => {
+    setError(null);
+    setIsLoading(true);
+
+    try {
+      const showData = await getActivity(Number(activityId));
+
+      setActivity(showData);
+    } catch (showDataError) {
+      setError(showDataError);
+    }
+    setIsLoading(false);
+  }, [activityId]);
+
+  React.useEffect(() => {
+    loadData();
+  }, [loadData]);
+
+  const handleActivityEdit = React.useCallback(() => {
+    navigate(`/activities/${activityId}/edit`);
+  }, [navigate, activityId]);
+
+  const handleActivityDelete = React.useCallback(async () => {
+    if (!activity) {
+      return;
+    }
+
+    const confirmed = await dialogs.confirm(
+      `Do you wish to delete ${activity.name}?`,
+      {
+        title: `Delete activity?`,
+        severity: 'error',
+        okText: 'Delete',
+        cancelText: 'Cancel',
+      },
+    );
+
+    if (confirmed) {
+      setIsLoading(true);
+      try {
+        await deleteActivity(Number(activityId));
+
+        navigate('/activities');
+
+        notifications.show('Activity deleted successfully.', {
+          severity: 'success',
+          autoHideDuration: 3000,
+        });
+      } catch (deleteError) {
+        notifications.show(
+          `Failed to delete activity. Reason:' ${deleteError.message}`,
+          {
+            severity: 'error',
+            autoHideDuration: 3000,
+          },
+        );
+      }
+      setIsLoading(false);
+    }
+  }, [activity, dialogs, activityId, navigate, notifications]);
+
+  const handleBack = React.useCallback(() => {
+    navigate('/activities');
+  }, [navigate]);
+
+  const renderShow = React.useMemo(() => {
+    if (isLoading) {
+      return (
+        <Box
+          sx={{
+            flex: 1,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            width: '100%',
+            m: 1,
+          }}
+        >
+          <CircularProgress />
+        </Box>
+      );
+    }
+    if (error) {
+      return (
+        <Box sx={{ flexGrow: 1 }}>
+          <Alert severity="error">{error.message}</Alert>
+        </Box>
+      );
+    }
+
+    return activity ? (
+      <Box sx={{ flexGrow: 1, width: '100%' }}>
+        <Grid container spacing={2} sx={{ width: '100%' }}>
+          <Grid size={{ xs: 12, sm: 6 }}>
+            <Paper sx={{ px: 2, py: 1 }}>
+              <Typography variant="overline">Name</Typography>
+              <Typography variant="body1" sx={{ mb: 1 }}>
+                {activity.name}
+              </Typography>
+            </Paper>
+          </Grid>
+          <Grid size={{ xs: 12, sm: 6 }}>
+            <Paper sx={{ px: 2, py: 1 }}>
+              <Typography variant="overline">Age</Typography>
+              <Typography variant="body1" sx={{ mb: 1 }}>
+                {activity.age}
+              </Typography>
+            </Paper>
+          </Grid>
+          <Grid size={{ xs: 12, sm: 6 }}>
+            <Paper sx={{ px: 2, py: 1 }}>
+              <Typography variant="overline">Join date</Typography>
+              <Typography variant="body1" sx={{ mb: 1 }}>
+                {dayjs(activity.joinDate).format('MMMM D, YYYY')}
+              </Typography>
+            </Paper>
+          </Grid>
+          <Grid size={{ xs: 12, sm: 6 }}>
+            <Paper sx={{ px: 2, py: 1 }}>
+              <Typography variant="overline">Department</Typography>
+              <Typography variant="body1" sx={{ mb: 1 }}>
+                {activity.role}
+              </Typography>
+            </Paper>
+          </Grid>
+          <Grid size={{ xs: 12, sm: 6 }}>
+            <Paper sx={{ px: 2, py: 1 }}>
+              <Typography variant="overline">Full-time</Typography>
+              <Typography variant="body1" sx={{ mb: 1 }}>
+                {activity.isFullTime ? 'Yes' : 'No'}
+              </Typography>
+            </Paper>
+          </Grid>
+        </Grid>
+        <Divider sx={{ my: 3 }} />
+        <Stack direction="row" spacing={2} justifyContent="space-between">
+          <Button
+            variant="contained"
+            startIcon={<ArrowBackIcon />}
+            onClick={handleBack}
+          >
+            Back
+          </Button>
+          <Stack direction="row" spacing={2}>
+            <Button
+              variant="contained"
+              startIcon={<EditIcon />}
+              onClick={handleActivityEdit}
+            >
+              Edit
+            </Button>
+            <Button
+              variant="contained"
+              color="error"
+              startIcon={<DeleteIcon />}
+              onClick={handleActivityDelete}
+            >
+              Delete
+            </Button>
+          </Stack>
+        </Stack>
+      </Box>
+    ) : null;
+  }, [
+    isLoading,
+    error,
+    activity,
+    handleBack,
+    handleActivityEdit,
+    handleActivityDelete,
+  ]);
+
+  const pageTitle = `Activity ${activityId}`;
+
+  return (
+    <PageContainer
+      title={pageTitle}
+      breadcrumbs={[
+        { title: 'Activities', path: '/activities' },
+        { title: pageTitle },
+      ]}
+    >
+      <Box sx={{ display: 'flex', flex: 1, width: '100%' }}>{renderShow}</Box>
+    </PageContainer>
+  );
+}
